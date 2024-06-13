6655ccd6d104bb09e6f263ccbd430c9095e57306
oscarevc <o.valdebenito02@ufromail.cl>
2024-06-13 15:49:17 -0400

    inicio

--- archivo.py
+++ archivo.py
@@ -1,3 +1,25 @@
-#hola a todos, este es el principio del proyecto
+import json
+import os
 
-#cambio 2.0 l codigo
+# Clases
+class Evento:
+    def __init__(self, nombre, fecha, hora):
+        self.nombre = nombre
+        self.fecha = fecha
+        self.hora = hora
+
+class Competidor:
+    def __init__(self, nombre, edad, categoria):
+        self.nombre = nombre
+        self.edad = edad
+        self.categoria = categoria
+
+class SportPlanningManager:
+    def __init__(self):
+        self.eventos = []
+        self.competidores = []
+
+    def agregar_evento(self, nombre, fecha, hora):
+        evento = Evento(nombre, fecha, hora)
+        self.eventos.append(evento)
+        print("Evento agregado exitosamente.")
